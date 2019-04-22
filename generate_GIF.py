260d69bc73f3092b90b6f864aa72a2d960e6d85b
lintian <17210720082@fudan.edu.cn>
2019-04-22 19:52:03 +0800

    initialization

---
+++ generate_GIF.py
@@ -0,0 +1,13 @@
+import os
+import imageio
+
+images = []
+dir_name = r'images/'
+# sort the images by number value
+images_name = [x for x in os.listdir(dir_name)]
+images_name.sort(key=lambda x: int(x[:-4]))
+images_name = [dir_name + x for x in images_name]
+images_name = images_name[0:35]
+for i in images_name:
+    images.append(imageio.imread(i))
+imageio.mimsave('demo.gif', images, duration=0.3)
