36ae8192e1bb6f41a2323de524ef4157562ead16
lintian <17210720082@fudan.edu.cn>
2019-04-22 20:21:48 +0800

    change GIF

--- generate_GIF.py
+++ generate_GIF.py
@@ -7,7 +7,7 @@ dir_name = r'images/'
 images_name = [x for x in os.listdir(dir_name)]
 images_name.sort(key=lambda x: int(x[:-4]))
 images_name = [dir_name + x for x in images_name]
-images_name = images_name[0:10]
+images_name = images_name[0:20]
 for i in images_name:
     images.append(imageio.imread(i))
 imageio.mimsave('demo.gif', images, duration=0.3)
